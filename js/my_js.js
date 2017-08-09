5e505205f9a4ff38d5e57c8202308ca6fb44e1b0
HidingDark <1041035312@qq.com>
2017-08-09 14:54:49 +0800

    change code

--- js/my_js.js
+++ js/my_js.js
@@ -1,3 +1,21 @@
 /**
  * Created by Administrator on 2017/8/8.
  */
+$(document).ready(function () {
+    initData();
+})
+function initData() {
+    var htmlStr="";
+    var article=blogJson.article;
+    for(var i in article){
+        var content="";
+        var item=article[i];
+        for (var j in item.content){
+            content+="<p>"+item.content[j]+"</p>";
+        }
+        htmlStr+="<div class='aritcle-container'><a class='read-more' href='readmore.html?id="+i+"'>...ReadMore</a><h2>"+item.head+"</h2>"+
+            '<div><img class="article-img col-md-4 col-lg-4 col-xs-4" src="'+item.img+'">'+
+            "<div class='article-content col-md-8 col-lg-8 col-xs-8'"+">"+content+"</div>"+"</div>"+"</div>";
+    }
+    $(".article-list").html(htmlStr);
+}
--- js/readmore.js
+++ js/readmore.js
@@ -1,3 +1,41 @@
 /**
  * Created by Administrator on 2017/8/8.
  */
+$(document).ready(function () {
+    var article=blogJson.article;
+    var id=parseInt(GetQueryString("id"));
+    init(article,id);
+    $("#pre").on("click",function () {
+        if(id<=0){
+            id=article.length-1;
+        }else{
+            id-=1;
+        }
+        init(article,id);
+    });
+    $("#next").on("click",function () {
+        if(id>=(article.length-1)){
+            id=0;
+        }else{
+            id+=1;
+        }
+        init(article,id);
+    });
+})
+function GetQueryString(name)
+{
+    var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
+    var r = window.location.search.substr(1).match(reg);
+    if(r!=null)return  unescape(r[2]); return null;
+}
+function init(article,id){
+    var content="";
+    var htmlStr="";
+    for (var i in article[id].content){
+        content+="<p>"+article[id].content[i]+"</p>";
+    }
+    htmlStr+="<div class='aritcle-container'><h2>"+article[id].head+"</h2>"+
+        '<img class="article-img" src="'+article[id].img+'">'+
+        "<div class='article-content'"+">"+content+"</div>"+"</div>";
+    $(".article-list").html(htmlStr);
+}
